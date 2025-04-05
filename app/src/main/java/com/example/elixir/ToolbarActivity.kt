cbb2f55baca9f888b2aca859a638ccf7c3801601
Doyun-Rec <166380967+Doyun-Rec@users.noreply.github.com>
2025-04-06 02:42:59 +0900

    feat: add xml files of login/signup
    - login activity
    : enter, id/pw, buttons(login/signup)
    - registration activity
    : toolbar(back button, title, more button)
    - add string(birth year)
    - fix not selected string
    
    * it will be used not only in signup, but also record of diet/recipe / detail mypage
    - info fragment : enter id, pw, nickname, profile photo, sex, birth year
    - survey fragments(1~4)

---
+++ app/src/main/java/com/example/elixir/ToolbarActivity.kt
@@ -0,0 +1,52 @@
+package com.example.elixir
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.fragment.app.Fragment
+import com.example.elixir.databinding.ActivityToolbarBinding
+
+class ToolbarActivity : AppCompatActivity() {
+    // 선언부
+    private lateinit var toolBinding: ActivityToolbarBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // 화면 전체 사용, 상태 바를 투명하게 하기
+        enableEdgeToEdge()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        super.onCreate(savedInstanceState)
+        // 바인딩 정의
+        toolBinding = ActivityToolbarBinding.inflate(layoutInflater)
+        setContentView(toolBinding.root)
+
+        // 전 액티비티에서 정보 불러오기
+        // 어떤 모드인지 확인하고, 맞는 화면 띄워주기
+        val mode = intent.getIntExtra("mode", 0)
+        when(mode) {
+            // 회원가입 모드
+            1 -> {
+                // 툴바의 제목, 더보기 버튼 안보이게, 작동 x
+                toolBinding.title.visibility = View.INVISIBLE
+                toolBinding.btnMore.visibility = View.INVISIBLE
+
+                // 뒤로가기 버튼을 누르면 로그인 페이지로 돌아가기
+                // 돌아가기 전 다이얼로그 띄우기
+                toolBinding.btnBack.setOnClickListener {
+                    AlertExitDialogFragment(this).show()
+                }
+
+                // 정보 입력 프래그먼트 띄워주기
+                setFragment(SignupInfoFragment())
+            }
+        }
+    }
+
+    private fun setFragment(frag: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_registration, frag)
+            .commit()
+    }
+}
