037b4be5c78e5a5cf8417dff8127eb6a73d0928e
Doyun-Rec <166380967+Doyun-Rec@users.noreply.github.com>
2025-04-12 10:52:25 +0900

    feat: add gson, retrofit libs
    add them in gradle file for communication with network

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,4 +70,6 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation(libs.androidx.fragment.ktx)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
 }
