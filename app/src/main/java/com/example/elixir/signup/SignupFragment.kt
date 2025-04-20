04b6d9e5ab5c11421690fe22c2e593a149e44baf
Doyun-Rec <166380967+Doyun-Rec@users.noreply.github.com>
2025-04-20 13:01:27 +0900

    feat: add Survey3Fragment / refactor SignupFragment
    - add Survey3Fragment
    - refactor SignupFragment

--- app/src/main/java/com/example/elixir/signup/SignupFragment.kt
+++ app/src/main/java/com/example/elixir/signup/SignupFragment.kt
@@ -13,6 +13,13 @@ import com.example.elixir.databinding.FragmentSignupBinding
 class SignupFragment : Fragment() {
     private lateinit var signupBinding: FragmentSignupBinding
     private val userModel: UserInfoViewModel by activityViewModels()
+    private var profileData: ProfileData? = null            // 프로필 데이터
+    private var selectedAllergies: List<String>? = null     // 알러지 배열
+    private var preferredDiet: String? = null               // 선호 식단
+    private var preferredRecipe: List<String>? = null       // 선호 레시피
+
+
+    data class ProfileData(val img: String, val nick: String, val gender: String, val birthYear: Int)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -24,94 +31,167 @@ class SignupFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        with(signupBinding) {
 
-        // 상단 인디케이터 고정
-        setSurveyFragment(SurveyIndicatorFragment())
-        signupBinding.btnNext.isEnabled = false
-        // 처음: 프로필 설정 로딩
-        setSurveyStepFragment(userModel.currentStep)
+            // 상단 인디케이터 고정
+            setStepFragment(SurveyIndicatorFragment())
+            btnNext.isEnabled = false
+            // 처음: 프로필 설정 로딩
+            setSurveyStepFragment(userModel.currentStep)
 
-        // 이전 버튼 클릭 시
-        signupBinding.btnPrev.setOnClickListener {
-            // 프로필 설정 페이지면 계정 생성 페이지로
-            if (userModel.currentStep == 0) {
-                parentFragmentManager.beginTransaction()
-                    .replace(R.id.fragment_registration, CreateAccountFragment())
-                    .commit()
+            // 이전 버튼 클릭 시
+            btnPrev.setOnClickListener {
+                // 프로필 설정 페이지면 계정 생성 페이지로
+                if (userModel.currentStep == 0) {
+                    parentFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_registration, CreateAccountFragment())
+                        .commit()
+                }
+                // 아니면 설문 이전 단계로
+                else {
+                    userModel.currentStep -= 1
+                    setSurveyStepFragment(userModel.currentStep)
+                }
             }
-            // 아니면 설문 이전 단계로
-            else {
-                userModel.currentStep -= 1
-                setSurveyStepFragment(userModel.currentStep)
-            }
-        }
 
-        // 만약 정보 입력을 마친 상태면 다음 버튼 활성화
-        signupBinding.btnNext.setBackgroundTintList(
-            ContextCompat.getColorStateList(
-                requireContext(),
-                if (userModel.isStepCompleted())
-                    R.color.elixir_orange
-                else
-                    R.color.elixir_gray
-            )
-        )
-
-        // 다음 버튼 클릭 시
-        signupBinding.btnNext.setOnClickListener {
-            // 최대 단계 누르면 로그인 액티비티로
-            if (userModel.currentStep == userModel.maxStep) {
-                // 마지막 단계면 로그인 액티비티로 이동
-                activity?.finish()
-            }
-            // 아니면 설문 다음 단계로
-            else {
-                userModel.currentStep += 1
-                setSurveyStepFragment(userModel.currentStep)
+            // 다음 버튼 클릭 시
+            btnNext.setOnClickListener {
+                // 최대 단계 누르면 로그인 액티비티로
+                if (userModel.currentStep == userModel.maxStep) {
+                    // 마지막 단계면 로그인 액티비티로 이동
+                    activity?.finish()
+                }
+                else {
+                    // userModel의 프로필에 저장, 완료 스테이지 추가
+                    when(userModel.currentStep) {
+                        0 -> profileData?.let {
+                            userModel.setProfile(it.img, it.nick, it.gender, it.birthYear)
+                            userModel.updateCompletedStep(0, true)
+                        }
+                        1 -> selectedAllergies?.let {
+                            userModel.setAllergies(it)
+                            userModel.updateCompletedStep(1, true)
+                        }
+                        2 -> preferredDiet?.let {
+                            userModel.setPreferredDiets(it)
+                        }
+                    }
+                    // 설문 다음 단계로
+                    userModel.currentStep += 1
+                    setSurveyStepFragment(userModel.currentStep)
+                }
             }
         }
     }
-
-    private fun setSurveyFragment(fragment: Fragment) {
+    // 설문 프래그먼트 설정
+    private fun setStepFragment(fragment: Fragment) {
         childFragmentManager.beginTransaction()
             .replace(R.id.survey_step, fragment)
             .commit()
     }
-
+    // 설문 프래그먼트 설정
     private fun setSurveyStepFragment(step: Int) {
         val fragment = when (step) {
+            // 프로필 입력 프래그먼트, 칩 선택 시 다음 버튼 활성화
             0 -> SettingProfileFragment().apply {
                 listener = object : OnProfileCompletedListener {
+                    // 유효한 값이 입력되었다면 활성화
                     override fun onProfileCompleted(img: String, nick: String, sex: String, birthYear: Int) {
-                        userModel.setProfile(img, nick, sex, birthYear)
-                        userModel.updateCompletedStep(step, true)
-                        signupBinding.btnNext.isEnabled = true
+                        profileData = ProfileData(img, nick, sex, birthYear)
+                        // 버튼 활성화
+                        with(signupBinding) {
+                            btnNext.isEnabled = true
+                            btnNext.setBackgroundTintList(
+                                ContextCompat.getColorStateList(requireContext(), R.color.elixir_orange))
+                        }
                     }
+                    // 유효한 값이 입력되지 않았다면 활성화 x
                     override fun onProfileInvalid() {
-                        userModel.updateCompletedStep(step, false)
-                        signupBinding.btnNext.isEnabled = false
+                        profileData = null
+                        // 버튼 비활성화
+                        with(signupBinding) {
+                            btnNext.isEnabled = false
+                            btnNext.setBackgroundTintList(
+                                ContextCompat.getColorStateList(requireContext(), R.color.elixir_gray))
+                        }
                     }
                 }
                 signupBinding.surveyStep.visibility = View.GONE
             }
+            // 알레르기 입력 프래그먼트, 칩 선택 시 다음 버튼 활성화
             1 -> Survey1Fragment().apply {
                 listener = object : OnChipCompletedListener {
-                    override fun onChipSelected(isValid: Boolean) {
-                        // 버튼 활성화, 색상은 주황색으로
-                        signupBinding.btnNext.isEnabled = true
-                        signupBinding.btnNext.setBackgroundTintList(ContextCompat.getColorStateList(requireContext(), R.color.elixir_orange))
+                    // 칩이 선택되었다면, 다음 버튼 활성화 및 데이터 저장
+                    override fun onChipSelected(chips: List<String>) {
+                        selectedAllergies = chips
+                        // 버튼 활성화
+                        with(signupBinding) {
+                            btnNext.isEnabled = true
+                            btnNext.setBackgroundTintList(
+                                ContextCompat.getColorStateList(requireContext(), R.color.elixir_orange))
+                        }
                     }
-
-                    override fun onChipSelectedNot(isValid: Boolean) {
-                        // 버튼 비활성화, 색상은 회색으로
-                        signupBinding.btnNext.isEnabled = false
-                        signupBinding.btnNext.setBackgroundTintList(ContextCompat.getColorStateList(requireContext(), R.color.elixir_gray))
+                    // 칩이 선택되지 않았다면, 다음 버튼 비활성화 및 데이터 NULL
+                    override fun onChipSelectedNot() {
+                        selectedAllergies = null
+                        // 버튼 비활성화
+                        with(signupBinding) {
+                            btnNext.isEnabled = false
+                            btnNext.setBackgroundTintList(
+                                ContextCompat.getColorStateList(requireContext(), R.color.elixir_gray))
+                        }
                     }
-
                 }
             }
-            2 -> Survey2Fragment()
-            3 -> Survey3Fragment()
+            // 식단 입력 프래그먼트, 식단 선택 시 다음 버튼 활성화
+            2 -> Survey2Fragment().apply {
+                listener = object  : OnDietCompletedListener {
+                    // 식단이 입력되었다면, 다음 버튼 활성화 및 데이터 저장
+                    override fun onDietSelected(diet: String) {
+                        preferredDiet = diet
+                        // 버튼 활성화
+                        with(signupBinding) {
+                            btnNext.isEnabled = true
+                            btnNext.setBackgroundTintList(
+                                ContextCompat.getColorStateList(requireContext(), R.color.elixir_orange))
+                        }
+                    }
+                    // 식단이 선택되지 않았다면, 다음 버튼 비활성화 및 데이터 NULL
+                    override fun onDietSelectedNot() {
+                        preferredDiet = null
+                        // 버튼 비활성화
+                        with(signupBinding) {
+                            btnNext.isEnabled = false
+                            btnNext.setBackgroundTintList(
+                                ContextCompat.getColorStateList(requireContext(), R.color.elixir_gray))
+                        }
+                    }
+                }
+            }
+            3 -> Survey3Fragment().apply {
+                listener = object : OnChipCompletedListener {
+                    // 칩이 선택되었다면, 다음 버튼 활성화 및 데이터 저장
+                    override fun onChipSelected(chips: List<String>) {
+                        preferredRecipe = chips
+                        // 버튼 활성화
+                        with(signupBinding) {
+                            btnNext.isEnabled = true
+                            btnNext.setBackgroundTintList(
+                                ContextCompat.getColorStateList(requireContext(), R.color.elixir_orange))
+                        }
+                    }
+                    // 칩이 선택되지 않았다면, 다음 버튼 비활성화 및 데이터 NULL
+                    override fun onChipSelectedNot() {
+                        preferredRecipe = null
+                        // 버튼 비활성화
+                        with(signupBinding) {
+                            btnNext.isEnabled = false
+                            btnNext.setBackgroundTintList(
+                                ContextCompat.getColorStateList(requireContext(), R.color.elixir_gray))
+                        }
+                    }
+                }
+            }
             4 -> Survey4Fragment()
             else -> return
         }
--- app/src/main/java/com/example/elixir/signup/Survey3Fragment.kt
+++ app/src/main/java/com/example/elixir/signup/Survey3Fragment.kt
@@ -9,6 +9,8 @@ import com.example.elixir.databinding.FragmentSurvey3Binding
 
 class Survey3Fragment : Fragment() {
     private lateinit var survey3Binding: FragmentSurvey3Binding
+    private var preferredRecipes = mutableListOf<String>()
+    var listener: OnChipCompletedListener? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,5 +19,52 @@ class Survey3Fragment : Fragment() {
         // 바인딩
         survey3Binding = FragmentSurvey3Binding.inflate(inflater, container, false)
         return survey3Binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(survey3Binding) {
+            // 일반 칩
+            val chipList = listOf(dietKorean, dietChinese, dietJapanese, dietWestern, dessert,
+                beverage, sauce)
+            // 해당 없음
+            val chipNone = dietNA
+
+            // 일반 칩 클릭 시
+            chipList.forEach { chip ->
+                chip.setOnCheckedChangeListener { _, isChecked ->
+                    // 해당 없음 해제
+                    if (isChecked) {
+                        chipNone.isChecked = false
+                        preferredRecipes.clear()
+                        // 만약에 리스트에 같은 재료가 없다면 추가
+                        if (!preferredRecipes.contains(chip.text.toString())) {
+                            preferredRecipes.add(chip.text.toString())
+                        }
+                    }// 만약에 리스트에 같은 재료가 있다면 삭제
+                    else {
+                        preferredRecipes.remove(chip.text.toString())
+                    }
+                    // 업데이트
+                    updateSelection()
+                }
+            }
+            // "해당 없음" 클릭 시 나머지 해제
+            chipNone.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    chipList.forEach { it.isChecked = false }
+                    updateSelection()
+                }
+            }
+        }
+    }
+
+    private fun updateSelection() {
+        // 공백이 아니라면 칩이 선택된 상태 -> 다음 버튼 활성화
+        if (preferredRecipes.isNotEmpty()) {
+            listener?.onChipSelected(preferredRecipes)
+        } else {
+            listener?.onChipSelectedNot()
+        }
     }
 }
