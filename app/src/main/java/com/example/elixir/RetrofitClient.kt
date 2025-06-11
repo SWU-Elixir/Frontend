93b5ed0e96a2e49d663ef99702d19762263c982a
Zen <z_zen@naver.com>
2025-06-11 21:20:08 +0900

    fix: merge error

--- app/src/main/java/com/example/elixir/RetrofitClient.kt
+++ app/src/main/java/com/example/elixir/RetrofitClient.kt
@@ -26,7 +26,7 @@ import org.threeten.bp.LocalDateTime
 object RetrofitClient {
     // 서버 주소
     private const val BASE_URL = "https://port-0-elixir-backend-g0424l70py8py.gksl2.cloudtype.app/"
-    private var authToken: String? = "eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJBQGV4YW1wbGUuY29tIiwiYXV0aCI6IlVTRVIiLCJleHAiOjE3NDk2NDQ3NTh9.ECDJAzAt47yx_xe0xX-tDkku7LzV2PWY-eQ7_e_hh5b_iyA7fm_B5zRWl99akqTm" // Bearer 토큰을 저장할 변수
+    private var authToken: String? = "" // Bearer 토큰을 저장할 변수
     private var isRefreshing = false // 토큰 갱신 중인지 여부를 추적하는 플래그
 
     fun setAuthToken(token: String?) {
